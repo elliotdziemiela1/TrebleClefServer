b36328b09c8f6f6e5a525103ce12464061f578e8
Your Name <elliotanthony9@gmail.com>
2026-07-29 14:55:04 -0500

    removed trailing slash from cors origin.

--- index.ts
+++ index.ts
@@ -114,7 +114,7 @@ const dynamo_document_client = DynamoDBDocumentClient.from(dynamo_client);
 
 
 const app = express();
-app.use(cors({origin: ["http://localhost:5173", "https://treble-clef.vercel.app/"], credentials: true}))
+app.use(cors({origin: ["http://localhost:5173", "https://treble-clef.vercel.app"], credentials: true}))
 app.use(express.json());
 app.use(verifyAuth);
 
